cd79944e00d0787f1a2d88c2df67a509800f2bcb
Katrincik <grebennicova.ecaterina@gmail.com>
2024-11-10 23:33:48 +0200

    Changed the placements of console log

--- Lab2/Proxy/GymRegistrationProxy.js
+++ Lab2/Proxy/GymRegistrationProxy.js
@@ -8,8 +8,8 @@ class GymRegistrationProxy {
 
     register(user, trainerName = null) {
         if (this.isAdmin) {
-            this.gymRegistration.register(user, trainerName);
             console.log(`Admin is registering user: ${user.name}`);
+            this.gymRegistration.register(user, trainerName);
         } else {
             console.log("Access denied: Only admins can register new users.");
         }
@@ -17,8 +17,8 @@ class GymRegistrationProxy {
 
     displayAllUsers() {
         if (this.isAdmin) {
-            this.gymRegistration.displayAllUsers();
             console.log("Admin is viewing all user groups:");
+            this.gymRegistration.displayAllUsers();
         } else {
             console.log("Access denied: Only admins can view all users.");
         }
